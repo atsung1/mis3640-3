d88442529ce00997abeb85ef1cf67bb8351f8afe
Jingyu (Jack) He <jhe1@babson.edu>
2018-09-03 01:02:34 -0400

    Submission of Assignment 1

--- session01/calc.py
+++ session01/calc.py
@@ -1,1 +1,15 @@
-print(8 + 30 + 2018)
+#Assignment 1 for Class 1
+
+print("Q1")
+print(42*60+42)
+
+print("Q2")
+print(10/1.61)
+
+print("Q3")
+
+print("Pace, Seconds per mile")
+print((42*60+42)/(10/1.61))
+
+print("Speed, Miles per second")
+print((10/1.61)/(42*60+42))
--- session01/hello.py
+++ session01/hello.py
@@ -1,1 +1,16 @@
 print("Hello, Jack!")
+
+#Q1
+print("Hello, world!"
+
+#Q2
+print("Hello, world!)
+
+#Q3
+print(+2)
+print(2++2)
+#Q4
+print(02)
+
+#Q5
+print(32 12)
