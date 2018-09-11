2f6c764b4a5a3e53ecdb2eb64aa423913c420790
Jingyu (Jack) He <jhe1@babson.edu>
2018-09-11 16:31:53 -0400

    finished class 4

---
+++ session04/functiondemo.py
@@ -0,0 +1,34 @@
+# def print_lyrics():
+#     print('Hey Jude. Don\'t make it bad.')
+#     print('Take a sad song and make it better.')
+
+# print_lyrics()
+
+# print(type(print_lyrics))
+
+
+
+# def repeat_lyrics():
+#     print_lyrics()
+#     print('Na- na- na-!')
+#     print_lyrics()
+
+
+# repeat_lyrics()
+
+# def print_twice(whatever):
+#     print('first time:')
+#     print(whatever)
+#     print('the second time:')
+#     print(whatever)
+
+# print_twice('babson')
+
+# name = input('Please enter your name:')
+
+# print_twice(name)
+
+def give_me_a_break():
+    return 'break'
+
+print(give_me_a_break())
---
+++ session04/my_abs.py
@@ -0,0 +1,7 @@
+import math
+
+def my_abs(number):
+    result = math.sqrt(number ** 2)
+    print(result)
+
+my_abs(-77)
