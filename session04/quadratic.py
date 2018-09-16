3f5b4db4f2c28441ce7460e9bc3db1e5bc9284b5
Jingyu (Jack) He <jhe1@babson.edu>
2018-09-15 20:57:42 -0400

    HW for class 5

--- session04/quadratic.py
+++ session04/quadratic.py
@@ -2,14 +2,16 @@ import math
 
 def quadratic(a,b,c):
     
+    from math import sqrt
+
     a = float(a)
     b = float(b)
     c = float(c)
     d= b**2 - 4*a*c  
   
     if d >= 0:
-        x1 = (-b+math.sqrt((b**2)-(4*(a*c))))/(2*a)
-        x2 = (-b-math.sqrt((b**2)-(4*(a*c))))/(2*a)
+        x1 = (-b+sqrt((b**2)-(4*(a*c))))/(2*a)
+        x2 = (-b-sqrt((b**2)-(4*(a*c))))/(2*a)
         print ("This equation has two solutions: ", x1, x2)
 
     else:
---
+++ session05/turtledemo.py
@@ -0,0 +1,47 @@
+# import turtle
+# from math import pi
+
+# # jack = turtle.Turtle()
+
+# # for i in range(360):
+
+# #     jack.fd(1)
+# #     jack.lt(1)
+
+# # turtle.mainloop()
+
+
+# def polygon(t, length, n):
+#     for i in range(n):
+#         t.fd(length)
+#         t.lt(360/n)
+
+
+
+# # polygon(jack, 5, 10)
+
+
+# def circle(t, r):
+#     polygon(t, (2*pi*r)/360, 360)
+
+
+# # jack = turtle.Turtle()
+
+# # circle(jack, 100)
+
+import turtle
+import math
+
+def arc(t, r, angle):
+    arc_length = 2 * math.pi * r * angle / 360
+    n = int(arc_length / 3) + 1
+    step_length = arc_length / n
+    step_angle = angle / n
+    
+    for i in range(n):
+        t.fd(step_length)
+        t.lt(step_angle)
+
+jack = turtle.Turtle()
+arc(jack, 20, 90)
+turtle.mainloop()
